8b832a825503de5cd885f2c9055e9433645e5f6b
Richard Forward <richard_forward@hotmail.com>
2025-04-23 11:37:36 +0100

    Simplified deployment: removed package.json, server.js, deploy.sh and api directory; updated vercel.json for single file deployment

--- api/server.js
+++
@@ -1,7 +0,0 @@
-// Proxy api endpoint for Vercel serverless functions
-const requestHandler = require('../server');
-
-module.exports = (req, res) => {
-    // Pass to the common request handler
-    return requestHandler(req, res);
-}; 
